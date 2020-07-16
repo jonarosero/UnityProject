a6024c706dd761b63e4f648b703d6b130083e74f
Ascendere_Lab <ascendere_lab@iMac-de-Charlie.local>
2020-07-16 16:16:03 -0500

    8080 port change

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const app = express();
 
 app.use(express.static(__dirname + '/public/'));
 
-app.listen('3000', function() {
-  console.log('Servidor web escuchando en el puerto 3000');
-});
+app.listen(process.env.Port || 8080, () => console.log('All is ok'));
